f2fe309cc08a6d79c038869b1d76ed3dee91e530
Wouter van Kuipers <vankuipers@zorgdomein.nl>
2020-07-28 20:09:29 +0200

    Updated application code & implemented development/production database set-up

--- src/route/default.py
+++ src/route/default.py
@@ -1,10 +1,20 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, send_from_directory
 from flask_restful import Resource
 import config
 
 default = Blueprint('default', __name__)
 
+
 class Default(Resource):
-  @default.route('/', methods=['GET'])
-  def reportStatus():
-    return jsonify({'api': 'Gadget', 'version': config.version })
+    @default.route('/<path:path>', methods=['GET'])
+    def static_proxy(path):
+        return send_from_directory('static/gadget/', path)
+
+    @default.errorhandler(404)
+    @default.route('/')
+    def root(*args):
+        return send_from_directory('static/gadget/', 'index.html')
+
+    @default.route('/version', methods=['GET'])
+    def reportStatus():
+        return jsonify({'api': 'Gadget', 'version': config['Version']})
